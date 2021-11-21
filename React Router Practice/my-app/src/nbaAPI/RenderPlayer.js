4535be37a1680e1dfe1d6a6de14aec9c3194029e
jleonen <jleonen2012@gmail.com>
2021-11-20 23:24:11 -0800

    Added function to fetch data from API for first and last name of player that is being searched

--- React Router Practice/my-app/src/nbaAPI/RenderPlayer.js
+++ React Router Practice/my-app/src/nbaAPI/RenderPlayer.js
@@ -1,11 +1,47 @@
+import { useState } from "react";
 import SearchBar from "./SearchBar";
+import SearchResults from "./SearchResults";
 
 const RenderPlayer = () => {
-  const searchHandler = (data) => {
-    console.log(data);
+  const [results, setResults] = useState([]);
+  const searchHandler = async (data) => {
+    const searchData = data.split(" ").join("%20");
+    console.log(searchData);
+    try {
+      const response = await fetch(
+        `https://free-nba.p.rapidapi.com/players?page=0&per_page=25&search=${searchData}`,
+        {
+          method: "GET",
+          headers: {
+            "x-rapidapi-host": "free-nba.p.rapidapi.com",
+            "x-rapidapi-key":
+              "5ac30b084bmsh0cbed315ffdbb37p1a4bd6jsn5191d37aa887",
+          },
+        }
+      );
+      console.log(response);
+      const result = await response.json();
+      setResults([...result.data]);
+      results.map((item) => {
+        // console.log(item.first_name);
+        console.log(item.last_name);
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div>
+      <ul>
+        {results.map((player) => (
+          <li>
+            <SearchResults
+              firstName={player.first_name}
+              lastName={player.last_name}
+            />
+          </li>
+        ))}
+      </ul>
       <SearchBar onSearch={searchHandler} />
     </div>
   );
---
+++ React Router Practice/my-app/src/nbaAPI/SearchResults.js
@@ -0,0 +1,10 @@
+const SearchResults = (props) => {
+  return (
+    <div>
+      <span> {props.firstName} </span>
+      <span> {props.lastName}</span>
+    </div>
+  );
+};
+
+export default SearchResults;
